7959a1daf6240700fb92f5072e4a1abcff60de68
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-08-02 10:48:05 +0300

    first commit

--- client/src/TrelloApi.js
+++ client/src/TrelloApi.js
@@ -1,15 +1,11 @@
 import axios from 'axios';
 
-const apiKey = 'c504324bdddf4f0e60e1630435c46774';
-const token = 'ATTA1c040e32058e8c7b314e259c707fb7c93157266b2caa6fcee73647c9e7aae4dc18F09CAE';
-const boardId = 'ATATT3xFfGF00I4qBpaIRWbLweNLegwZ8jOslI3lCYRwCnsNdbET0zyfU2uBs-6Z9DMILzdsqwFMAsiSLVDOICrsNB8kVuqolzB7CbG7PEIwUVTUZy2Bc8INp1wcQEaQtEvfJdYLGtmlLK-l5s0PAQ90vSeVs5mEXYAG4S8C6OAUTRcn9mA1V90=AC54D593';
-
 const baseUrl = 'https://api.trello.com/1';
 
 export const createTodoCard = async (name, description) => {
   try {
-    const response = await axios.post(`${baseUrl}/cards?key=${apiKey}&token=${token}`, {
-      idList: boardId,
+    const response = await axios.post(`${baseUrl}/cards?key=${process.env.apiKey}&token=${process.env.token}`, {
+      idList: process.env.boardId,
       name,
       desc: description,
     });
@@ -23,7 +19,7 @@ export const createTodoCard = async (name, description) => {
 
 export const getTodoCards = async () => {
   try {
-    const response = await axios.get(`${baseUrl}/boards/${boardId}/cards?key=${apiKey}&token=${token}`);
+    const response = await axios.get(`${baseUrl}/boards/${process.env.boardId}/cards?key=${process.env.apiKey}&token=${process.env.token}`);
     return response.data;
   } catch (error) {
     console.error('Error getting cards:', error);
